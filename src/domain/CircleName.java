57e74575c4fe4da70855c028212899af5658ca73
woojoovove <ujinjeong55@gmail.com>
2024-10-21 15:13:44 +0900

    [add] VO CircleId, VO CircleName

---
+++ src/domain/CircleName.java
@@ -0,0 +1,16 @@
+package domain;
+
+public class CircleName {
+    private final String value;
+
+    public CircleName(String value) {
+        if (value == null) throw new IllegalArgumentException("value cannot be null");
+        if (value.length()<3) throw new IllegalArgumentException("Circle name length must be at least 3");
+        if (value.length()>20) throw new IllegalArgumentException("Circle name length must not be longer than 20");
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
