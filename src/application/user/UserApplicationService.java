95093ae4f09b1644c010c94c1efbecf532b04aa9
woojoovove <ujinjeong55@gmail.com>
2024-10-31 21:57:11 +0900

    [add] UserApplicationService
    find User Entity

---
+++ src/application/user/UserApplicationService.java
@@ -0,0 +1,17 @@
+package application.user;
+
+import domain.user.*;
+
+public class UserApplicationService {
+    private final IUserRepository userRepository;
+    private final IUserFactory userFactory;
+
+    public UserApplicationService(IUserRepository userRepository, IUserFactory userFactory) {
+        this.userRepository = userRepository;
+        this.userFactory = userFactory;
+    }
+
+    public User find(UserId userId) {
+        return userRepository.findOrNull(userId);
+    }
+}
