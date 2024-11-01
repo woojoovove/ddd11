afd8425cd146a10fb73b763b6e31d8cb912824ff
woojoovove <ujinjeong55@gmail.com>
2024-11-01 16:08:48 +0900

    [add] method 'joinCircle'

--- src/application/circle/CircleApplicationService.java
+++ src/application/circle/CircleApplicationService.java
@@ -1,9 +1,13 @@
-package application.circle.create;
+package application.circle;
 
+import application.circle.create.CircleCreateCommand;
+import application.circle.join.CircleJoinCommand;
 import application.user.UserApplicationService;
 import domain.circle.*;
 import domain.user.IUserRepository;
 import domain.user.User;
+
+import java.util.List;
 
 public class CircleApplicationService {
     private final ICircleRepository circleRepository;
@@ -19,11 +23,12 @@ public class CircleApplicationService {
 
     public void createCircle(CircleCreateCommand createCommand) {
         // CircleAppService가 UserRepository에 직접 의존하지 않도록 함.
-        User user = userApplicationService.find(createCommand.getOwnerId());
+        User user = userApplicationService.findOrNull(createCommand.getOwnerId());
         // TODO: 에러가 뷰 레이어에서 처리되었어야 하는 건 아닌가? 받자마자...
         if (user == null) throw new IllegalArgumentException("no circle leader");
         if (circleExists(createCommand.getName())) throw new IllegalArgumentException("circle already exists");
         Circle circle = circleFactory.create(createCommand.getName(), user);
+        circleRepository.save(circle);
     }
 
     // Entity가 이미 Repository에 존재하는지 확인하는 기능은
@@ -34,5 +39,18 @@ public class CircleApplicationService {
     // 앱 서비스로 구현한다.
     public boolean circleExists(CircleName circleName) {
         return circleRepository.findByNameOrNull(circleName) != null;
+    }
+
+    public void joinCircle(CircleJoinCommand joinCommand) {
+        Circle circle = circleRepository.findByIdOrNull(joinCommand.getCircleId());
+        if (circle == null) throw new IllegalArgumentException("circle not found");
+        User user = userApplicationService.findOrNull(joinCommand.getUserId());
+        if (user == null) throw new IllegalArgumentException("user not found");
+        List<User> member = circle.getMembers();
+        if (member.size()>29) {
+            throw new IllegalStateException("정원 초과");
+        }
+        member.add(user);
+        circleRepository.save(circle);
     }
 }
