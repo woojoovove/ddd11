5e33ab278237ea7b8147f36c993cc9bdec7c8b78
woojoovove <ujinjeong55@gmail.com>
2024-11-06 16:10:48 +0900

    [add] comment about intention of declaring find method in appService

--- src/application/user/UserApplicationService.java
+++ src/application/user/UserApplicationService.java
@@ -11,6 +11,10 @@ public class UserApplicationService {
         this.userFactory = userFactory;
     }
 
+    // 다른 도메인 서비스 (e.g. CircleAppllicationService)가
+    // leader를 세우기 위해서 받은 user가 존재하는 유저인지 판단하기 위해서
+    // UserRepository를 직접 사용하는 대신
+    // UserApplicationService가 user 존재 여부를 확인하는 메소드를 제공해주는 게 알맞음.
     public User findOrNull(UserId userId) {
         return userRepository.findOrNull(userId);
     }
