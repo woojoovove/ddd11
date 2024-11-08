6f280b6dbac5e703723dfcd3ad94ca4d0f8b07e2
woojoovove <ujinjeong55@gmail.com>
2024-11-08 16:23:41 +0900

    [add] user update command

---
+++ src/user/application/update/UserUpdateCommand.java
@@ -0,0 +1,14 @@
+package user.application.update;
+
+public class UserUpdateCommand {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
