a63536a806f54a9225906e872965ddeb45304765
woojoovove <ujinjeong55@gmail.com>
2024-11-12 15:20:16 +0900

    [add] UserRegisterCommand constructor

--- src/main/java/com/woojoovove/ddd11/user/application/register/UserRegisterCommand.java
+++ src/main/java/com/woojoovove/ddd11/user/application/register/UserRegisterCommand.java
@@ -3,6 +3,10 @@ package com.woojoovove.ddd11.user.application.register;
 public class UserRegisterCommand {
     private String userName;
 
+    public UserRegisterCommand(String userName) {
+        this.userName = userName;
+    }
+
     public String getUserName() {
         return userName;
     }
