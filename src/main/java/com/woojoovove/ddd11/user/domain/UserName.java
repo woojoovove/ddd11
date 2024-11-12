59adf686719100da8b699ed9cf9958dbb011c6c3
woojoovove <ujinjeong55@gmail.com>
2024-11-12 15:19:53 +0900

    [mod] typo 'circle'(X) -> 'user'(O)

--- src/main/java/com/woojoovove/ddd11/user/domain/UserName.java
+++ src/main/java/com/woojoovove/ddd11/user/domain/UserName.java
@@ -5,8 +5,8 @@ public class UserName {
 
     public UserName(String value) {
         if (value == null) throw new IllegalArgumentException("value cannot be null");
-        if (value.length()<3) throw new IllegalArgumentException("Circle name length must be at least 3");
-        if (value.length()>20) throw new IllegalArgumentException("Circle name length must not be longer than 20");
+        if (value.length()<3) throw new IllegalArgumentException("User name length must be at least 3");
+        if (value.length()>20) throw new IllegalArgumentException("User name length must not be longer than 20");
         this.value = value;
     }
 
