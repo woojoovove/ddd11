ec17d80195ae7dc2d8dbde4b14f1b3096eae371c
woojoovove <ujinjeong55@gmail.com>
2024-11-13 18:45:49 +0900

    [add] test cases for user class

---
+++ src/test/java/com/woojoovove/ddd11/user/domain/UserTest.java
@@ -0,0 +1,24 @@
+package com.woojoovove.ddd11.user.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class UserTest {
+    private UserId id;
+    private UserName name;
+    private User user;
+
+    public void failCreateUserWhenGivenNull() {
+
+    }
+
+    public void succeedCreateUserWhenGivenNotNull() {
+
+    }
+
+    public void failChangeNameWhenGivenNull() {}
+    public void succeedChangeNameWhenGivenNotNull() {}
+    public void cloneUserWithIndependentCopyWithSameValue() {}
+}
