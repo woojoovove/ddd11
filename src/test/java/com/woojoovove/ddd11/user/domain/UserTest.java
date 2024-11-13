3b36185b8ae0045f8acef92b0a927d82404faa6c
woojoovove <ujinjeong55@gmail.com>
2024-11-13 18:45:49 +0900

    [add] test cases for user class

---
+++ src/test/java/com/woojoovove/ddd11/user/domain/UserTest.java
@@ -0,0 +1,19 @@
+package com.woojoovove.ddd11.user.domain;
+
+public class UserTest {
+    private UserId id;
+    private UserName name;
+    private User user;
+
+    public void failCreateUserWhenGivenNull() {
+
+    }
+
+    public void succeedCreateUserWhenGivenNotNull() {
+
+    }
+
+    public void failChangeNameWhenGivenNull() {}
+    public void succeedChangeNameWhenGivenNotNull() {}
+    public void cloneUserWithIndependentCopyWithSameValue() {}
+}
