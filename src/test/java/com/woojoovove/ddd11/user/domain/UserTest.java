3eca28d41d50c2190a28c4d94fffb13e55b4f89d
woojoovove <ujinjeong55@gmail.com>
2024-11-13 18:50:29 +0900

    [add] user test - create with null id

--- src/test/java/com/woojoovove/ddd11/user/domain/UserTest.java
+++ src/test/java/com/woojoovove/ddd11/user/domain/UserTest.java
@@ -1,12 +1,21 @@
 package com.woojoovove.ddd11.user.domain;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class UserTest {
     private UserId id;
     private UserName name;
     private User user;
 
-    public void failCreateUserWhenGivenNull() {
-
+    @Test
+    public void failCreateUserWhenGivenNullId() {
+        id = null;
+        name = new UserName("ABC");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> User.create(id, name));
+        assertEquals("id cannot be null", exception.getMessage());
     }
 
     public void succeedCreateUserWhenGivenNotNull() {
