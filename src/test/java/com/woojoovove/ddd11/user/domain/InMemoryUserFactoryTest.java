b5386490387956031578ef3900b01fd8c977f220
woojoovove <ujinjeong55@gmail.com>
2024-11-14 15:56:42 +0900

    [add] UserFactory - succeed create user

---
+++ src/test/java/com/woojoovove/ddd11/user/domain/InMemoryUserFactoryTest.java
@@ -0,0 +1,26 @@
+package com.woojoovove.ddd11.user.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class InMemoryUserFactoryTest {
+
+    private InMemoryUserFactory userFactory;
+    @BeforeEach
+    public void setUp() {
+        userFactory = new InMemoryUserFactory();
+    }
+    @Test
+    public void succeedCreateUser() {
+        UserName userName = new UserName("userName");
+        User user = userFactory.create(userName);
+        assertNotNull(user);
+        assertNotNull(user.getId());
+        assertDoesNotThrow(()->UUID.fromString(user.getId().getValue()));
+        assertEquals(userName, user.getName());
+    }
+}
