9b9844d35489f937a80df8ba7fa7c1acc990b9d0
woojoovove <ujinjeong55@gmail.com>
2024-11-15 17:39:06 +0900

    [add] test UserRepository - findAll()

--- src/test/java/com/woojoovove/ddd11/user/domain/InMemoryUserRepositoryTest.java
+++ src/test/java/com/woojoovove/ddd11/user/domain/InMemoryUserRepositoryTest.java
@@ -1,7 +1,10 @@
 package com.woojoovove.ddd11.user.domain;
 
+import com.woojoovove.ddd11.user.application.common.UserData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,5 +31,19 @@ public class InMemoryUserRepositoryTest {
     public void returnNullGivenNonExistUser() {
         UserId id = new UserId("AAA");
         assertNull(repository.findOrNull(id));
+    }
+
+    @Test
+    public void returnAllUser(){
+        User user1= User.create(new UserId("aaa"), new UserName("bbb"));
+        User user2= User.create(new UserId("ccc"), new UserName("ddd"));
+        repository.save(user1);
+        repository.save(user2);
+
+        List<UserData> users = repository.findAll().stream().map(UserData::new).toList();
+        assertEquals(2, users.size());
+        assertTrue(users.contains(new UserData(user1)));
+        assertTrue(users.contains(new UserData(user2)));
+
     }
 }
