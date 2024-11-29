3a29516061db26feb430d92da72f649a4aae251c
woojoovove <ujinjeong55@gmail.com>
2024-11-29 09:47:08 +0900

    [add] test CircleName - fail create with too short

--- src/test/java/com/woojoovove/ddd11/circle/domain/CircleNameTest.java
+++ src/test/java/com/woojoovove/ddd11/circle/domain/CircleNameTest.java
@@ -7,9 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CircleNameTest {
     @Test
-    public void failCreateGivenNull() {
+    public void failCreateCircleNameGivenNull() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
                 ()-> new CircleName(null));
         assertEquals(exception.getMessage(), "value cannot be null");
+    }
+
+    @Test
+    public void failCreateCircleNameGivenTooShort() {
+        String tooShort = "ab";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                ()-> new CircleName(tooShort));
+        assertEquals(exception.getMessage(), "Circle name length must be at least 3");
     }
 }
